bcd0da3467c8939101853d9e44a098a4e58d19d0
Joe Pollock <cjosephpollock@gmail.com>
2016-05-31 17:58:03 -0700

    fixed registration bug where it wouldnt load the map after registering

--- Flowww/app/src/main/java/com/example/iguest/flowww/SignUpActivity.java
+++ Flowww/app/src/main/java/com/example/iguest/flowww/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.iguest.flowww;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
@@ -30,15 +32,28 @@ public class SignUpActivity extends AppCompatActivity {
 
                 Log.v("blah", "creating an account!!!!");
 
-                EditText username = (EditText) findViewById(R.id.username);
-                EditText password = (EditText) findViewById(R.id.password);
+                final EditText username = (EditText) findViewById(R.id.username);
+                final EditText password = (EditText) findViewById(R.id.password);
 
 
                 ref.createUser(username.getText().toString(), password.getText().toString(), new Firebase.ValueResultHandler<Map<String, Object>>() {
                 @Override
                 public void onSuccess(Map<String, Object> result) {
                         System.out.println("Successfully created user account with uid: " + result.get("uid"));
+                        ref.authWithPassword(username.getText().toString(), password.getText().toString(), new Firebase.AuthResultHandler() {
+                        @Override
+                        public void onAuthenticated(AuthData authData) {
+                            System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+                            Intent intent = new Intent(SignUpActivity.this, MapActivity.class);
+                            //intent.putExtra("EXTRA_TEXT", null); // not sure what string to use here...
+                            startActivity(intent);
                         }
+                        @Override
+                        public void onAuthenticationError(FirebaseError firebaseError) {
+                            // there was an error
+                        }
+                    });
+                }
                 @Override
                 public void onError(FirebaseError firebaseError) {
                         // there was an error
